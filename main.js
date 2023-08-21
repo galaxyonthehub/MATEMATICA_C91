e36becf9379dff8cac806c9aee9e3d4b002afe1a
galaxyonthehub <117865433+galaxyonthehub@users.noreply.github.com>
2023-08-21 17:32:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function  addUser() 
+{
+  player1_name = localStorage.getItem("player1_name");
+  player2_name = localStorage.getItem("player2_name");
+      window.location = "Test.html";
+
+}
---
+++ test.js
@@ -0,0 +1,33 @@
+
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score ;
+document.getElementById("player2_score").innerHTML = player2_score ;
+
+document.getElementById("player_question").innerHTML = "Turno de Pergunta - " + player1_name ;
+document.getElementById("player_answer").innerHTML = "Turno de Resposta - " + player2_name ;
+
+function send() {
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+
+    question_number = "<h4>" + number1 + "X" + number2 + "</h4>";
+    input_box = "<br>Resposta : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Checar</button>";
+    row =  question_number + input_box + check_button ; 
+    document.getElementById("output").innerHTML = row;
+
+}
+
+
+question_turn = "player1";
+answer_turn = "player2";
+
